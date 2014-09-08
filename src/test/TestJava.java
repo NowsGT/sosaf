56a95698b12e386173746cdb6980a9c546fca38f
nasminspy <nasminmora@gmail.com>
2014-09-08 22:29:03 +0530

    test java

---
+++ src/test/TestJava.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by nasmin on 9/8/14.
+ */
+public class TestJava {
+    public static void main(String [ ] args){
+        System.out.println("Hello Java!");
+    }
+}
