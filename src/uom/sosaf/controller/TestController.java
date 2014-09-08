fc7a5dadf998cc2f8c171b6790cb93a0f488f106
nasminspy <nasminmora@gmail.com>
2014-09-08 21:29:50 +0530

    controller java

---
+++ src/uom/sosaf/controller/TestController.java
@@ -0,0 +1,10 @@
+package uom.sosaf.controller;
+
+/**
+ * Created by nasmin on 9/8/14.
+ */
+public class TestController {
+    public static void main(String [ ] args){
+        System.out.println("Hello Test!");
+    }
+}
